d0335a31c01d5e389845da0f2123be49b0dc4cdb
Yotlan <yotlanlecrom@yahoo.fr>
2022-11-20 14:26:24 +0100

    update federapp

--- utils/compile-and-run-federapp.sh
+++ utils/compile-and-run-federapp.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-# Parameters 1 is for the configuration file which RDF4J need to consedering named graph as endpoint
+set -x
 
-PARAM1=$1
-
-# Parameters 2 is for the SPARQL query
-
-PARAM2=$2
-
-# Parameters 3 is for the result of the query
-
-PARAM3=$3
-
-# Parameters 4 is for the statistics of the query
-
-PARAM4=$4
-
-# Parameters 5 is for the source selection he used
-# Parameters 6 is for the number of HTTP request he do
-# Parameters 7 is for the log file of the query's execution
-
-mkdir -p "$(dirname "$3")" && touch "$3"
 mkdir -p "$(dirname "$4")" && touch "$4"
 mkdir -p "$(dirname "$5")" && touch "$5"
 mkdir -p "$(dirname "$6")" && touch "$6"
+mkdir -p "$(dirname "$7")" && touch "$7"
 
-cd Federapp
-mvn install dependency:copy-dependencies package
-java -Xmx32384m -Xms32384m -classpath "target/Federapp-1.0-SNAPSHOT.jar:target/lib/*" org.example.Federapp "$@"
+cd Federapp || exit 1
+mvn install dependency:copy-dependencies package || exit 1
+
+if [ "$1" -lt "0" ]; then
+  echo "Timeout disabled"
+  java -classpath "target/Federapp-1.0-SNAPSHOT.jar:target/lib/*" org.example.Federapp "${@:2}"
+else
+  echo "Timeout enabled"
+  timeout --signal=SIGKILL "$1" java -classpath "target/Federapp-1.0-SNAPSHOT.jar:target/lib/*" org.example.Federapp "${@:2}"
+fi
+
+EXIT_STATUS=$?
+echo "EXIT_STATUS = $EXIT_STATUS"
+
+
+if [ $EXIT_STATUS -eq 0 ]
+then
+    echo 'All went fine :)'
+    exit 0
+fi
+
+if [ $EXIT_STATUS -eq 137 ]
+then
+    echo 'Process Timed Out!'
+    echo "query,exec_time,total_distinct_ss,nb_http_request,total_ss" > "$5"
+    echo "$3,failed,failed,failed,failed" >> "$5"
+    exit 0
+fi
+
+echo "Unknown error :("
+echo "query,exec_time,total_distinct_ss,nb_http_request,total_ss" > "$5"
+echo "$3,failed,failed,failed,failed" >> "$5"
+
+exit 0
